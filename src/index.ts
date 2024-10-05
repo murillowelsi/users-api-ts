42e1fb039384387b5f4542553afb330df15f5033
Murillo Welsi <murillo.welsi@gmail.com>
2024-10-05 22:05:31 +0100

    feat: add express inital server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!!!");
+import { config } from "dotenv";
+import express from "express";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}! 🚀`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
