b74d8f6254b0c0ab14261ab85965a50e21a4da56
Murillo Welsi <murillo.welsi@gmail.com>
2024-10-05 22:37:06 +0100

    feat: add GetUsersController

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,3 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}! 🚀`);
 });
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
