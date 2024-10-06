5caeb36bffcbd4dc3d7b2588dbf6c592ddde76e7
Murillo Welsi <murillo.welsi@gmail.com>
2024-10-06 11:22:15 +0100

    feat: refactor Omit

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -5,8 +5,7 @@ export const MongoClient = {
   db: undefined as unknown as Db,
 
   async connect(): Promise<void> {
-    const url =
-      process.env.MONGODB_URL || "mongodb+srv://cluster0.jpvtp.mongodb.net";
+    const url = process.env.MONGODB_URL || "mongodb://localhost:27017";
     const username = process.env.MONGODB_USERNAME;
     const password = process.env.MONGODB_PASSWORD;
 
